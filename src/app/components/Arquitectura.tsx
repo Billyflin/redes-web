74440c4125627448c14133691d0e49b458082470
Billy <Billymartinezc@gmail.com>
2024-12-24 12:49:00 -0300

    Elimina importación innecesaria de 'next/image'
    Se removió la importación de 'next/image' en el componente Arquitectura.tsx ya que no se utiliza. Esto optimiza el código y elimina redundancias innecesarias.

--- src/app/components/Arquitectura.tsx
+++ src/app/components/Arquitectura.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Mermaid from "@/app/components/Mermaid"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
